3f4494770887b03820d0cf4471acad69ac1bb746
Neelam-zanvar <neelam.zanvar@minutuscomputing.com>
2022-06-13 17:45:57 +0530

    changed python

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,1 +1,13 @@
-python --version
+# redirect stderr into stdout
+$p = &{python -V} 2>&1
+# check if an ErrorRecord was returned
+$version = if($p -is [System.Management.Automation.ErrorRecord])
+{
+    # grab the version string from the error message
+    $p.Exception.Message
+}
+else 
+{
+    # otherwise return as is
+    $p
+}
