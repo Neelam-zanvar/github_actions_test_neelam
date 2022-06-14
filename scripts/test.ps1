d90876af84f17276b61d971b9695f218d41e13b0
Neelam-zanvar <neelam.zanvar@minutuscomputing.com>
2022-06-14 16:39:45 +0530

    changed python

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,3 +1,4 @@
 [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-$elevated = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-Write-Host $elevated
+# $elevated = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+# Write-Host $elevated
+Invoke-CimMethod -ClassName Win32_Product -MethodName Install -Arguments @{PackageLocation='https://notepad-plus-plus.org/downloads/v8.4.1/'}
