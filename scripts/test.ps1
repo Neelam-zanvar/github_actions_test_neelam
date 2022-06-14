f11c633f4f8f51e191cbd30fa7b63f6ae84d9988
Neelam-zanvar <neelam.zanvar@minutuscomputing.com>
2022-06-14 15:00:26 +0530

    changed python

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,4 +1,3 @@
 [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 New-Item -ItemType directory -Path C:\newDir
-pip install -r requirements.txt
-streamlit run app.py
+python-3.9.0.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
