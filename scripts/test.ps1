332e01d9a3429043e770529b13a205d1c65cbce7
Neelam-zanvar <neelam.zanvar@minutuscomputing.com>
2022-06-14 15:47:49 +0530

    changed python

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,2 +1,3 @@
 [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 $elevated = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+Write-Host $elevated
