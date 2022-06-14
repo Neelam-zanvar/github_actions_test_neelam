5325be81e78844abcb55843bfdda797350dd64b7
Neelam-zanvar <neelam.zanvar@minutuscomputing.com>
2022-06-14 15:42:22 +0530

    changed python

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,2 +1,2 @@
 [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-Start-Process msiexec.exe -Wait -ArgumentList '/I C:\Users\lateweb.info\Desktop\Notepad++7_8_9.msi /quiet'Start-Process msiexec.exe -Wait -ArgumentList '/I C:\Users\lateweb.info\Desktop\Notepad++7_8_9.msi /quiet'
+whoami
