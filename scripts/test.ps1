c0ab11d4096fcd3e34607978cfe71a34e97f690d
Neelam-zanvar <neelam.zanvar@minutuscomputing.com>
2022-06-14 15:13:01 +0530

    changed python

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,3 +1,3 @@
 [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 New-Item -ItemType directory -Path C:\newDir
-python-3.9.0.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+winget install -e --id Notepad++.Notepad++
