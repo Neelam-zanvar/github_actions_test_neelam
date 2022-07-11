0c59da381df333e92f6970a3c24353959841d7a4
Neelam-zanvar <98389856+Neelam-zanvar@users.noreply.github.com>
2022-07-11 14:47:46 +0530

    Update test.ps1

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,4 +1,3 @@
-[System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-# $elevated = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-# Write-Host $elevated
-Invoke-CimMethod -ClassName Win32_Product -MethodName Install -Arguments @{PackageLocation='https://notepad-plus-plus.org/downloads/v8.4.1/'}
+Write-Output "Hello world"
+New-Item D:\test.txt
+Set-Content D:\test.txt 'Welcome to github actions'
